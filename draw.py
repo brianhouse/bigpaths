cf696fb44dd8a9522f72dd1470de71e60a5967e3
brianhouse <brian.house@gmail.com>
2017-05-12 20:02:41 -0400

    refactored with numpy for auto size calc

--- draw.py
+++ draw.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import random
+import numpy as np
 from bson.son import SON
 from housepy import drawing, geo, config, log
 from mongo import db
@@ -17,33 +18,33 @@ USER_ID = None
 MILES = 20
 SIZE = 1
 
-
 if USER_ID is not None:
     users = 1
     results = db.entries.find({'user_id': USER_ID, 'location': {'$near': {'$geometry': CITY, '$maxDistance': 1609 * MILES}}})
 else:
     users = len(db.entries.distinct('user_id'))
-    # results = db.entries.find({'location': {'$near': {'$geometry': CITY, '$maxDistance': 1609 * MILES}}})
-    results = db.entries.find()
+    results = db.entries.find({'location': {'$near': {'$geometry': CITY, '$maxDistance': 1609 * MILES}}})
 
 log.info("USERS %s" % users)
 log.info("POINTS %s" % results.count())
 
-min_x, max_y = geo.project((-180, 85))
-max_x, min_y = geo.project((180, -85))
+points = np.array([(result['location']['coordinates'][0], result['location']['coordinates'][1], result['user_id']) for result in results])
 
-# min_x, max_y = geo.project((-75, 42))
-# max_x, min_y = geo.project((-73, 40))
+min_lon, max_lon = (np.min(points[:,0]), np.max(points[:,0]))
+min_lat, max_lat = (np.min(points[:,1]), np.max(points[:,1]))
+log.debug("%f %f %f %f" % (min_lon, max_lon, min_lat, max_lat))
+
+min_x, max_y = geo.project((min_lon, max_lat))
+max_x, min_y = geo.project((max_lon, min_lat))
 
 ratio = (max_x - min_x) / (max_y - min_y)
 
 ctx = drawing.Context(10000, int(10000 / ratio), relative=True, flip=True, hsv=True)
-log.info("%d %d" % (ctx.width, ctx.height))
+log.info("Drawing %d %d..." % (ctx.width, ctx.height))
 
-for result in results:
+for point in points:
 
-    lon, lat, alt = result['location']['coordinates']
-    x, y = geo.project((lon, lat))
+    x, y = geo.project((point[0], point[1]))
 
     if x > max_x or x < min_x or y > max_y or y < min_y:
         continue
@@ -51,7 +52,7 @@ for result in results:
     x = (x - min_x) / (max_x - min_x)
     y = (y - min_y) / (max_y - min_y)
 
-    ctx.arc(x, y, SIZE / ctx.width, SIZE / ctx.height, fill=(colors[result['user_id'] % len(colors)]), thickness=0.0)
+    ctx.arc(x, y, SIZE / ctx.width, SIZE / ctx.height, fill=(colors[int(point[2]) % len(colors)]), thickness=0.0)
 
 ctx.output("maps")
 
