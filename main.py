3b6ff60771f5ee0f45c0eff5c08c0db426d31c5a
brianhouse <brian.house@gmail.com>
2017-06-02 12:49:12 -0400

    refactoring data files

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ from sequencer import sequence
 from housepy import util
 
 
-# user_ids = util.load("user_ids.pkl")
-# # user_ids = [1]  # me
+user_ids = util.load("data/user_ids.pkl")
+# user_ids = [1]  # me
 
-# sequences = sequence(user_ids)
+corpus = sequence(user_ids)
 
-# util.save("sequences.pkl", sequences)
+util.save("data/corpus.pkl", corpus)
 
 
-sequences = util.load("sequences_house.pkl")
+corpus = util.load("data/corpus_house.pkl")
 
 d = 80
-drawer.sequences(sequences[(d * 144):(d * 144) + 144])
+drawer.sequences(corpus[(d * 144):(d * 144) + 144])
--- train_test.py
+++ train_test.py
@@ -8,10 +8,41 @@ from keras.layers.recurrent import LSTM
 from keras.layers.core import Dense, Activation, Dropout
 from keras.callbacks import ModelCheckpoint
 from keras.utils import plot_model
+from data import *
 
 WEIGHTS = None
 if len(sys.argv) > 1:
     WEIGHTS = sys.argv[1]
+
+corpus = util.load("sequences_house.pkl")
+
+# split the dataset into moving sequences
+log.info("Generating training sequences...")
+sequence_length = PERIODS
+sequences = []
+outputs = []
+for i in range(0, len(corpus) - sequence_length):
+    sequences.append(corpus[i:i + sequence_length])  # this is the sequence
+    outputs.append(corpus[i + sequence_length])   # the expected output is the next lonlat
+log.info("--> %d sequences" % len(sequences)) 
+
+# make an empty 3-tensor for the input sequences
+X = np.zeros((len(sequences), sequence_length, 2), dtype=np.float)
+
+# make an empty tensor for the output (which is just 2d, a lonlat for each input sequence)
+y = np.zeros((len(sequences), 2), dtype=np.bool)
+
+# set the appropriate indices to 1 in each one-hot vector
+for i, sequence in enumerate(sequences):
+    for t, character in enumerate(sequence):
+        X[i, t, character_to_label[character]] = 1
+    y[i, character_to_label[outputs[i]]] = 1
+
+
+
+exit()
+
+
 
 log.info("Creating model...")
 model = Sequential()
