32fc75a65ba47e3f8fcc432137ac1df9fe62f0f5
brianhouse <brian.house@gmail.com>
2017-06-12 19:59:16 -0400

    created printer stub

---
+++ print.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import random, sys
+import numpy as np
+import drawer
+from housepy import config, log, geo
+from points import *
+
+days = util.load("data/1497311751_output.pkl")
+
+# for day in days:
+#     drawer.path(day)
+
+drawer.strips([point for day in days for point in day])
