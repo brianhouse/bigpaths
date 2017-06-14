d5d89811ed61ae4890ec90bfc09c0950fd84a52a
brianhouse <brian.house@gmail.com>
2017-06-14 19:21:37 -0400

    drawing and geocoding

--- print.py
+++ print.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import random, sys
+import random, sys, requests, json
 import numpy as np
 import drawer
 from housepy import config, log, geo
@@ -11,6 +11,39 @@ from points import *
 
 
 days = util.load("data/1497313211_10_7_house_0.25_output.pkl")
-day = random.choice(days)
+index = random.choice(range(len(days)))
+
+day = days[index] + days[index+1]
+
+def trim(day):
+    d = 0
+    while True:
+        if day[d].period < day[d+1].period:
+            d += 1
+        else:
+            d += 1
+            break    
+    day = day[d:]
+    d = 0
+    while True:
+        if day[d].period < day[d+1].period:
+            d += 1
+        else:
+            d += 1
+            break   
+    day = day[:d]
+    return day
+
+day = trim(day)
+
+geocode(day)
+format_times(day)
+
+print()
+print()
+for point in day:
+    print("%s: %s" % (point.display_time, point.address))
+
+
 drawer.path_print(day)
 
