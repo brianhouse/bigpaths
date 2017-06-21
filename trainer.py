f662d919a8893a0ca2ed9797e54351c6452b5dc9
brianhouse <brian.house@gmail.com>
2017-06-21 16:06:04 -0400

    added layer

--- trainer.py
+++ trainer.py
@@ -75,6 +75,7 @@ log.info("Creating model...")
 model = Sequential()
 model.add(LSTM(512, return_sequences=True, input_shape=(MEMORY, CATEGORIES)))   # no dropout, we dont really care about overfitting
 model.add(LSTM(512, return_sequences=True))
+model.add(LSTM(512, return_sequences=True))
 model.add(LSTM(512, return_sequences=False))
 model.add(Dense(CATEGORIES, activation="softmax"))
 if WEIGHTS is not None:
