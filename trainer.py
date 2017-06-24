943ec076b72a8dd8b39cfbd857dd45a6773e1b64
brianhouse <brian.house@gmail.com>
2017-06-24 18:37:02 -0400

    updated model

--- trainer.py
+++ trainer.py
@@ -73,9 +73,9 @@ X, y, period_refs = generate_input()
 # create model
 log.info("Creating model...")
 model = Sequential()
-model.add(LSTM(512, return_sequences=True, input_shape=(MEMORY, CATEGORIES)))   # no dropout, we dont really care about overfitting
-model.add(LSTM(512, return_sequences=True))
-model.add(LSTM(512, return_sequences=False))
+model.add(LSTM(1024, return_sequences=True, input_shape=(MEMORY, CATEGORIES)))   # no dropout, we dont really care about overfitting
+model.add(LSTM(1024, return_sequences=True))
+model.add(LSTM(1024, return_sequences=False))
 model.add(Dense(CATEGORIES, activation="softmax"))
 if WEIGHTS is not None:
     model.load_weights(WEIGHTS)
