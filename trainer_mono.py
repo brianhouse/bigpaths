126918bde893e4c8febdb63baa0563d801255ac3
brianhouse <brian.house@gmail.com>
2017-07-05 11:45:32 -0400

    skipping generator

--- trainer_mono.py
+++ trainer_mono.py
@@ -40,24 +40,64 @@ model.summary()
 log.info("--> done")
 
 
-# input generator
-data = util.load(config['points'])
-epoch_size = len(data) - MEMORY
-def input_generator():
-    points = []
-    X = []
-    y = []
-    for p, point in enumerate(data):
-        points.append(point)
-        if len(points) < MEMORY + 1:
-            continue
-        X.append(to_categorical([point.location for point in points[:-1]], CATEGORIES))
-        y.append(to_categorical(points[-1].location, CATEGORIES)[0])
-        points = points[1:]
-        if len(y) == config['batch_size']:
-            yield np.array(X), np.array(y)
-            X = []
-            y = []
+# # input generator
+# data = util.load(config['points'])
+# epoch_size = len(data) - MEMORY
+# def input_generator():
+#     points = []
+#     X = []
+#     y = []
+#     for p, point in enumerate(data):
+#         points.append(point)
+#         if len(points) < MEMORY + 1:
+#             continue
+#         X.append(to_categorical([point.location for point in points[:-1]], CATEGORIES))
+#         y.append(to_categorical(points[-1].location, CATEGORIES)[0])
+#         points = points[1:]
+#         if len(y) == config['batch_size']:
+#             yield np.array(X), np.array(y)
+#             X = []
+#             y = []
+
+def generate_input():
+    log.info("Generating input sequences...")
+    points = util.load(config['points'])
+    cells = []
+    for point in points:
+        cells.append(point.location)
+        cells.append(LOCATIONS + point.duration)
+    inputs = []
+    outputs = []
+    period_refs = []
+    for i in range(0, len(cells[:]) - MEMORY):
+        inputs.append(cells[i:i + MEMORY])
+        outputs.append(cells[i + MEMORY])
+        period_refs.append(points[(i + MEMORY)//2].period)    # the period of the target, which we'll use to reconstruct the point when generating
+    cells = None
+    log.info("--> %d points into %s sequences" % (len(points), len(inputs)))    
+    log.info("Making categorical inputs (%s memory required)..." % strings.format_size(len(inputs) * MEMORY * CATEGORIES))
+    input_length = (len(inputs) // BATCH_SIZE) * BATCH_SIZE # we need inputs to be a multiple of batch_size so we dont train on multiple users in the same batch
+    X = np.zeros((input_length, MEMORY, CATEGORIES), dtype=np.bool)
+    y = np.zeros((input_length, CATEGORIES), dtype=np.bool)
+    last_percent = 0
+    for i, input in enumerate(inputs):
+        if i == input_length:
+            break
+        X[i] = to_categorical(np.array(input), CATEGORIES)
+        percent = int((i / input_length) * 100)
+        if percent != last_percent:
+            util.progress_bar(percent)
+        last_percent = percent
+    inputs = None
+    log.info("--> complete")
+    log.info("Categorializing output...")
+    outputs = np.array(outputs[:len(X)])
+    for o, output in enumerate(outputs):
+        y[o][output] = 1
+    log.info("--> %d input vectors" % len(X))
+    log.info("--> shape: %s" % (X[0].shape,))
+    return X, y, period_refs
+X, y, period_refs = generate_input()
 
 
 # train
@@ -76,7 +116,8 @@ if train:
             callbacks = [ModelCheckpoint(filepath="models/%s-{epoch:02d}-{acc:.4f}.hdf5" % MODEL.strip("models/").strip(".hdf5"), verbose=1, save_best_only=True, monitor="acc", mode="max")]
         else:
             callbacks = None
-        model.fit_generator(input_generator(), epoch_size, epochs=config['epochs'], callbacks=callbacks)
+        # model.fit_generator(input_generator(), epoch_size, epochs=config['epochs'], callbacks=callbacks)
+        model.fit(X, y, epochs=config['epochs'], batch_size=config['batch_size'], callbacks=callbacks)
     except KeyboardInterrupt:
         print()
 if config['autonomous']:
