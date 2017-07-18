1f3d390f66e50c0db7e80047ada49b0e95df9dee
brianhouse <brian.house@gmail.com>
2017-07-18 19:32:29 -0400

    building out webservice

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 
-import subprocess, os, sys, datetime, urllib.parse, random
+import subprocess, os, sys, datetime, urllib.parse, random, markdown
 from housepy import config, log, timeutil, geo, server, jobs
 from itinerarier import Point
-
 
 class Home(server.Handler):
 
     def get(self, lonlat=None):
         if lonlat is None or not len(lonlat):
-            return self.text("OK")
+            try:
+                with open(os.path.abspath(os.path.join(os.path.dirname(__file__), "templates", "home.md"))) as f:
+                    text = f.read()
+                    readme = markdown.markdown(text)
+            except Exception as e:
+                log.error(log.exc(e))
+            return self.render("index.html", readme=readme)
         try:
             log.debug(lonlat)
             lonlat = lonlat.split(',')
@@ -18,13 +23,17 @@ class Home(server.Handler):
             current_geohash = [geo.geohash_encode(lonlat)]
             for i in range(8 - len(current_geohash[0])):
                 current_geohash.append(random.choice("0123456789bcdefghjkmnpqrstuvwxyz"))            
-            current_geohash = str("".join(current_geohash)).lstrip('dr')                
-            dt = timeutil.t_to_dt(tz="America/New_York")
-            current_period = ((dt.hour * 60) + (dt.minute)) // 10
-            log.info("Current geohash: %s" % current_geohash)
-            log.info("Current period: %d" % current_period)
-            seed = "".join(["%s:%s;" % (current_period - (2 - i), current_geohash) for i in range(3)])
-            log.info("Seed: %s" % seed)        
+            current_geohash = str("".join(current_geohash))
+            job_id = str(random.randint(0, 1000000))
+            self.jobs.add(tube="generate", data={'job_id': job_id, 'geohash': current_geohash})
+            result = []
+            def on_job(data):
+                if data['job_id'] == job_id:
+                    result.append(data['point'])
+            while not len(result):
+                self.jobs.process(handler=on_job, tube="receive", num_jobs=1)    
+            point = result.pop()                
+            return self.text("%s,%s" % (point.display_time, point.address))                
         except Exception as e:
             log.error(log.exc(e))
         return self.text("OK")
@@ -33,13 +42,4 @@ handlers = [
     (r"/?([^/]*)", Home),
 ]    
 
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("[model]")
-        exit()
-    slug = sys.argv[1].split('/')[-1].split('.')[0]
-    log.info("--> using model %s" % slug)
-    root = os.path.abspath(os.path.dirname(__file__))
-    jobs.launch_beanstalkd()
-    server.start(handlers)
+server.start(handlers)
---
+++ templates/home.md
@@ -0,0 +1,6 @@
+# Following piece
+<span id="status"><a href="javascript:generateLocation();">Generate next location</a></span>  
+
+<div id="spinner" class="spinner"></div>
+
+Research by [Brian House](http://brianhouse.net)
